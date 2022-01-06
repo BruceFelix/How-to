084abee17991265aaacf9bbdaf987e9113e05d5d
Bruce Felix <brucefelixm@gmail.com>
2022-01-06 11:49:24 +0300

    trying to create a slide show

---
+++ Slideshow/main.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides"); //gets the number of slide from the pagee
+    let dots = document.getElementsByClassName('dot'); //gets the dots from html
+
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+    else if(n < 1){
+        slideIndex = slideIndex.length;
+    }
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for( i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += 'active';
+
+}
